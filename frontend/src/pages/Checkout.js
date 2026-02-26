9a476e593fe2ed1e3b637181c4a1feb51eff24c4
khajurkart <khajurkart@gmail.com>
2026-02-27 02:16:38 +0530

    Update Checkout.js

--- frontend/src/pages/Checkout.js
+++ frontend/src/pages/Checkout.js
@@ -8,6 +8,8 @@ import { useRazorpay } from 'react-razorpay';
 
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 const API = `${BACKEND_URL}/api`;
+console.log("Backend URL:", BACKEND_URL);
+
 
 const Checkout = () => {
   const { cart, cartTotal, clearCart } = useCart();
