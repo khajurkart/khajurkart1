5e1aa4f70561edf94800dbf41a6bdca957a916dc
khajurkart <khajurkart@gmail.com>
2026-04-14 21:05:05 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "@/index.css";
 import App from "@/App";
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+<GoogleOAuthProvider clientId="982249580062-vk6flrc2s2uknmq75s71a613jdeu4etv.apps.googleusercontent.com">
+  <App />
+</GoogleOAuthProvider>
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
