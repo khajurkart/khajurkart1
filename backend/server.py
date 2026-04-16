ab8ee5db38f940d8185c02325841590be6e41e6c
khajurkart <khajurkart@gmail.com>
2026-04-16 20:13:09 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -347,7 +347,7 @@ async def register(user_data: UserRegister):
         "phone": user_data.phone,
         "role": "user",
         "verification_code": verification_code,                 # ✅ ADD
-        "is_verified": True,
+        "is_verified": False,
         "created_at": datetime.now(timezone.utc).isoformat()
     }
     
