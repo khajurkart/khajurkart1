84b61380189b3dc834280892e6dffd8e173ca050
khajurkart <khajurkart@gmail.com>
2026-04-20 12:47:27 +0530

    Update Footer.js

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -86,7 +86,7 @@ const Footer = () => {
               <Instagram className="w-5 h-5" />
             </a>
             <a href="#" className="text-khajur-cream hover:text-khajur-gold transition-colors">
-              <Pinterest className="w-5 h-5" />
+              <Twitter className="w-5 h-5" />
             </a>
           </div>
         </div>
