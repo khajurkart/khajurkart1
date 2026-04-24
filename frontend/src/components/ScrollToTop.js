3c2879ce00c812db7adfa8b70948221c5c19e0fd
khajurkart <khajurkart@gmail.com>
2026-04-24 16:37:14 +0530

    Update ScrollToTop.js

--- frontend/src/components/ScrollToTop.js
+++ frontend/src/components/ScrollToTop.js
@@ -2,13 +2,19 @@ import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 function ScrollToTop() {
-  const { pathname } = useLocation();
+    const { pathname } = useLocation();
 
-  useEffect(() => {
-    window.scrollTo({ top: 0, behavior: "instant" });
-  }, [pathname]);
+    useEffect(() => {
+        const container = document.querySelector(".your-scroll-container");
 
-  return null;
+        if (container) {
+            container.scrollTo(0, 0);
+        } else {
+            window.scrollTo(0, 0);
+        }
+    }, [pathname]);
+
+    return null;
 }
 
 export default ScrollToTop;
