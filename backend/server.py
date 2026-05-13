92f8836096962b90901f303ae28f7afa035896c7
khajurkart <khajurkart@gmail.com>
2026-05-14 00:11:19 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -994,7 +994,7 @@ async def add_to_cart(
                     "product_id": cart_item.product_id,
                     "quantity": cart_item.quantity,
                     "size": selectedSize   # ✅ FIXED
-                });
+                })
             }
         )
 
