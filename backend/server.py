c2fba1b3e149abb8c006f85cd5041dcaebfbb948
khajurkart <khajurkart@gmail.com>
2026-05-14 00:06:34 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -989,9 +989,12 @@ async def add_to_cart(
     if not item_exists:
         cart["items"].append(
             {
-                "product_id": cart_item.product_id,
-                "quantity": cart_item.quantity,
-                "size": selectedSize   # ✅ FIXED
+                
+                console.log({
+                    "product_id": cart_item.product_id,
+                    "quantity": cart_item.quantity,
+                    "size": selectedSize   # ✅ FIXED
+                });
             }
         )
 
