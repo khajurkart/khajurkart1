bc8f8217fa706a03fe9bcba69a139b56a7490b16
khajurkart <khajurkart@gmail.com>
2026-05-20 20:15:09 +0530

    Update CartContext.js

--- frontend/src/context/CartContext.js
+++ frontend/src/context/CartContext.js
@@ -7,7 +7,6 @@ const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 const API = `${BACKEND_URL}/api`;
 console.log("Backend URL:", BACKEND_URL);
 
-
 const CartContext = createContext();
 
 export const useCart = () => {
